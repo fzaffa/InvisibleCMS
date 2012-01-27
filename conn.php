f89fb200612c92a017c30809b59d6d7750e56f96
fzaffa <fraz.93@gmail.com>
2012-01-27 21:49:16 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "zaffaron";
-$db = "mycms";
+$host = "";
+$user = "";
+$pass = "";
+$db = "";
 mysql_connect($host, $user, $pass)
 	or die(mysql_error());
 mysql_select_db($db);
