29389a3315f7465c4e14783512e0a7210a8c9db4
Francesco Zaffaroni <francescoz@zaffanet.org>
2012-01-27 21:48:30 +0100

    001

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-$host = "yourhost";
-$user = "youruser";
-$pass = "yourpass";
-$db = "yourdatabase";
+$host = "localhost";
+$user = "root";
+$pass = "zaffaron";
+$db = "mycms";
 mysql_connect($host, $user, $pass)
 	or die(mysql_error());
 mysql_select_db($db);
