135a257c524efa476bcffaba262c53588e309a95
Francesco Zaffaroni <fraz.93>
2014-08-28 10:59:48 +0200

    Handled exception if filter does not exist

--- src/Controller.php
+++ src/Controller.php
@@ -11,8 +11,21 @@ class Controller {
 
     public function filter($filter)
     {
+        try {
+            $this->runFilter($filter);
+        } catch (Exception $e){
+            echo $e->getMessage();
+            exit(1);
+        }
+    }
+    public function runFilter($filter)
+    {
         $method = 'filter'.$filter;
-        $this->{$method}();
-
+        if(method_exists($this, $method))
+        {
+            $this->{$method}();
+        } else {
+            throw new Exception('Filter '.$filter.' does not exist.');
+        }
     }
 } 
